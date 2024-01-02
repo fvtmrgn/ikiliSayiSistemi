686a186987a8781b3f6d650dd2436b442397fbfa
Fatımatüzzehra Ergen <88490838+fvtmrgn@users.noreply.github.com>
2024-01-02 19:18:52 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İkiliSayiSistemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Onlu sayı sistemine çevrilecek ikili sayı sisteminde sayı giriniz;");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                int  ikiliDeger, onluDeger = 0, tabanDeger = 1, basamakDeger;
+                ikiliDeger = sayi;
+
+                while (sayi > 0)
+                {
+                    basamakDeger = sayi % 10;
+                    onluDeger = onluDeger + basamakDeger * tabanDeger;
+                    sayi = sayi / 10;
+
+                    tabanDeger = tabanDeger * 2;
+                }
+                Console.Write("İkili (Binary) Sayı: " + ikiliDeger);
+                Console.Write("\nOnlu (Decimal) Sayı: " + onluDeger);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
